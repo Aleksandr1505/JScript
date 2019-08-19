79c72b252a8b16ee0cf3c9b35a89ce380ec6a589
Aleksandr <olexander.o.havrylenko@gmail.com>
2019-08-19 12:53:39 +0300

    changed css and html

--- Projects/Project-1/js/fetch/fetch.js
+++ Projects/Project-1/js/fetch/fetch.js
@@ -1,9 +1,23 @@
+const refs = {
+  search: document.querySelector("#modal-search-form")
+};
 
-// fetch('https://api.themoviedb.org/3/movie/64/recommendations?api_key=1d42ad8865a0c243bfacfcefa347033e&language=ru&page=1')
-//   .then(response => response.json())
-//   .then(data => {
-//   console.log(data);
-//   })
-//   .catch(error => {
-//     console.log('Error :', error);
-//   });
+const baseUrl = "https://api.themoviedb.org/3";
+const myKey = "1d42ad8865a0c243bfacfcefa347033e";
+
+function fetchArticles(query) {
+  const request = `/search/${query}?api_key=${myKey}`;
+  return fetch(baseUrl + request).then(response =>
+    response.json()
+  );
+}
+refs.search.addEventListener("submit", searchMovieHandle);
+function searchMovieHandle(e) {
+  e.preventDefault();
+  console.dir(e.currentTarget);
+  const searchValue = e.currentTarget.elements.mdsearch.value;
+  console.log(searchValue);
+  fetchArticles(searchValue).then(data => {
+    console.log(data);
+  });
+}
