ff2bf681e32ae856b237e68852d016c062c4c344
Aleksandr <olexander.o.havrylenko@gmail.com>
2019-08-19 11:44:46 +0300

    changed js

---
+++ Projects/Project-1/js/fetch/fetch.js
@@ -0,0 +1,9 @@
+
+// fetch('https://api.themoviedb.org/3/movie/64/recommendations?api_key=1d42ad8865a0c243bfacfcefa347033e&language=ru&page=1')
+//   .then(response => response.json())
+//   .then(data => {
+//   console.log(data);
+//   })
+//   .catch(error => {
+//     console.log('Error :', error);
+//   });
--- Projects/Project-1/js/index.js
+++ Projects/Project-1/js/index.js
@@ -2,16 +2,16 @@ const input = document.querySelector("search-form__field");
 const first_button = document.querySelector(".first-button");
 const modalform = document.querySelector('.modal-form');
 const overlay = document.querySelector('.modal-overlay');
-console.log(first_button);
-console.log(modalform);
+const closeModalBtn = document.querySelector('button[data-action="close-modal-form"]');
 first_button.addEventListener('click', openModal);
 overlay.addEventListener('click', closeModal);
-
+closeModalBtn.addEventListener('click', closeModal);
 
 function openModal(e) {
   e.preventDefault();
   modalform.classList.add('is-open');
   overlay.classList.add('is-open');
+  window.addEventListener('keydown', handleButtonPress)
 }
 
 function closeModal() {
@@ -20,31 +20,9 @@ function closeModal() {
   window.removeEventListener('keydown', handleButtonPress)
 }
 
-// function handleClose (e) {
-//   if(e.target === lbImage) {
-//       return;
-//   }
-
-//   closeModal();
-// }
-
 function handleButtonPress(e) {
   if(e.code !== 'Escape') {
   return;
   }
-
   closeModal();
 }
-
-
-
-
-
-fetch('https://api.themoviedb.org/3/movie/64/recommendations?api_key=1d42ad8865a0c243bfacfcefa347033e&language=ru&page=1')
-  .then(response => response.json())
-  .then(data => {
-  console.log(data);
-  })
-  .catch(error => {
-    console.log('Error :', error);
-  });
