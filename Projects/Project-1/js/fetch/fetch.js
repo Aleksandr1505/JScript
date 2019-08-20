c1f5efa51215d87c8580f89f34c3c13a517459e0
Aleksandr <olexander.o.havrylenko@gmail.com>
2019-08-20 13:11:34 +0300

    added js

--- Projects/Project-1/js/fetch/fetch.js
+++ Projects/Project-1/js/fetch/fetch.js
@@ -6,18 +6,33 @@ const baseUrl = "https://api.themoviedb.org/3";
 const myKey = "1d42ad8865a0c243bfacfcefa347033e";
 
 function fetchArticles(query) {
-  const request = `/search/${query}?api_key=${myKey}`;
+  const request = `/search/movie?api_key=${myKey}&language=en-US&query=${query}`;
   return fetch(baseUrl + request).then(response =>
     response.json()
   );
 }
 refs.search.addEventListener("submit", searchMovieHandle);
+
 function searchMovieHandle(e) {
   e.preventDefault();
   console.dir(e.currentTarget);
-  const searchValue = e.currentTarget.elements.mdsearch.value;
-  console.log(searchValue);
-  fetchArticles(searchValue).then(data => {
-    console.log(data);
+  const form = e.currentTarget;
+  const inputtwo = form.elements.mdsearch;
+  const inputValue =  inputtwo.value;
+  inputtwo.value = '';
+  console.log(inputValue);
+  fetchArticles(inputValue).then(data => {
+        console.log(data);
   });
 }
+
+///////////////////////////////////////////////////////////
+// function searchMovieHandle(e) {
+//   e.preventDefault();
+//   console.dir(e.currentTarget);
+//   const searchValue = e.currentTarget.elements.mdsearch.value;
+//   console.log(searchValue);
+//   fetchArticles(searchValue).then(data => {
+//     console.log(data);
+//   });
+// }
